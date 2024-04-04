13d09896770c57837020c314d777d59abe514975
minhnguyengp1 <minhnguyengp1@gmail.com>
2024-04-04 16:04:01 +0200

    Refactor App.js and implement login

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,59 @@
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
-import Header from './components/Header.jsx';
-import Footer from './components/Footer.jsx';
-import './styles/main.scss';
-import React from 'react';
-import Home from './pages/Home.jsx';
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom'
+import Header from './components/Header.jsx'
+import Footer from './components/Footer.jsx'
+import './styles/main.scss'
+import React from 'react'
+import Home from './pages/Home.jsx'
+import Login from './pages/Login.jsx'
+import Register from './pages/Register.jsx'
 
-const Layout = ({ children }) => {
+const Layout = () => {
     return (
         <>
             <Header />
-            {children}
+            <Outlet />
             <Footer />
         </>
-    );
-};
+    )
+}
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Layout />,
+        children: [
+            {
+                path: '/',
+                element: <Home />,
+            },
+            // {
+            //     path: '/article/:id',
+            //     element: <Article />,
+            // },
+            // {
+            //     path: '/write',
+            //     element: <Write />,
+            // },
+        ],
+    },
+    {
+        path: '/register',
+        element: <Register />,
+    },
+    {
+        path: '/login',
+        element: <Login />,
+    },
+])
 
 function App() {
     return (
         <div className="app">
             <div className="container">
-                <Router>
-                    <Layout>
-                        <Routes>
-                            <Route path="/" element={<Home />} />
-                            {/* <Route
-                                path="/components"
-                                element={<Components />}
-                            /> */}
-                            {/* <Route path="/templates" element={<Templates />} /> */}
-                        </Routes>
-                    </Layout>
-                </Router>
+                <RouterProvider router={router} />
             </div>
         </div>
-    );
+    )
 }
 
-export default App;
+export default App
