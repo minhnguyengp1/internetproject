75c73bb14dd1bc5b3535bd6376795deefab87cbf
Buni55 <b.berber@live.de>
2024-04-24 19:05:26 +0200

    fixed some css

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,33 +1,43 @@
-import React from 'react';
-import "./headerStyle.scss"
+import React from 'react'
+import './headerStyle.scss'
 import AppLogo from '../assets/app-logo.png'
-import { Link } from 'react-router-dom';
-import { FaSearch } from 'react-icons/fa'; // Importiere das Lupe-Symbol von react-icons
+import { Link } from 'react-router-dom'
+import { FaSearch } from 'react-icons/fa' // Importiere das Lupe-Symbol von react-icons
 
 const Header = () => {
-    return(
-        <div>
+    return (
+        <div className="header">
             <div className="top">
-                <img className="imageLogo" src={AppLogo} alt=""/>
-        
+                <img className="imageLogo" src={AppLogo} alt="" />
+
                 <form className="buttonsTop">
-                    <button className="buttonRegister"> <span><Link to="/register"> Regestrieren </Link></span></button>
+                    <button className="buttonRegister">
+                        {' '}
+                        <span>
+                            <Link to="/register"> Regestrieren </Link>
+                        </span>
+                    </button>
                     <span>oder</span>
-                    <button className="buttonEinloggen"> <span><Link to="/login">Einloggen</Link></span></button>
+                    <button className="buttonEinloggen">
+                        {' '}
+                        <span>
+                            <Link to="/login">Einloggen</Link>
+                        </span>
+                    </button>
                 </form>
-        
             </div>
 
             <div className="down">
                 <form className="buttonsDown">
                     <input type="text" placeholder="was suchen Sie?" />
                     <input type="text" placeholder="PLZ oder Ort" />
-                    <button><FaSearch /> Finden</button> 
+                    <button>
+                        <FaSearch /> Finden
+                    </button>
                 </form>
             </div>
-
         </div>
-       );
-};
+    )
+}
 
-export default Header;
+export default Header
